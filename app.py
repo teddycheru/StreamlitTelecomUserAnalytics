01752740a8a2df8d508394d6bd6a9ed3a998c111
teddycheru <tedc21@yahoo.com>
2024-04-26 15:55:44 +0300

    Dashboard development - test

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Set the page configuration as the first Streamlit command
+st.set_page_config(page_title="My Streamlit App")
+
+# Your Streamlit app code goes here
+st.write('Hello World')
